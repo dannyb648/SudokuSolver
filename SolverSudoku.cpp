4d68163d9204a9036597f027fb743e1121fd17ae
Dan Beglin <dbeglinuk@gmail.com>
2017-02-01 22:34:41 +0000

    check in box and check are finished

--- SolverSudoku.cpp
+++ SolverSudoku.cpp
@@ -3,6 +3,7 @@ using std::cout;
 using std::endl;
 
 #define W 9
+#define L W*W
 #define EMPTY 0
 
 int getIndex(int y, int x)
@@ -10,7 +11,7 @@ int getIndex(int y, int x)
 	return (y * W) + x;
 }
 
-bool inX(int puzzle[81], int x, int target)
+bool inX(int puzzle[L], int x, int target)
 {
 	for(int y = 0; y < W; y++)
 	{
@@ -22,7 +23,7 @@ bool inX(int puzzle[81], int x, int target)
 	return false;
 }
 
-bool inY(int puzzle[81], int y, int target)
+bool inY(int puzzle[L], int y, int target)
 {
 	for(int x = 0; x < W; x++)
 	{
@@ -34,20 +35,44 @@ bool inY(int puzzle[81], int y, int target)
 	return false;
 }
 
+bool inB(int puzzle[L], int bX, int bY, int target)
+{
+	for(int x = 0; x < 3; x++)
+	{
+		for(int y = 0; y < 3; y++)
+		{
+			if(puzzle[getIndex(y + bY, x + bX)] == target)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool isLegal(int puzzle[L], int x, int y, int target)
+{
+	return !inX(puzzle, x, target)
+	 &&    !inY(puzzle, y, target)
+	 &&    !inB(puzzle, x - x%3, y - y%3, target);
+}
+
 int main(int argc, char *argv[])
 {
-	int col = 7;
-	int row = 4;
-	int puzzle[81] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 
-					  9, 10, 11, 12, 13, 14, 15, 16, 17,
-					  18, 19, 20, 21, 22, 23, 24, 25, 26, 
-					  27, 28, 29, 30, 31, 32, 33, 34, 35, 
-					  36, 37, 38, 39, 40, 41, 42, 43, 44, 
-					  45, 46, 47, 48, 49, 50, 51, 52, 53, 
-					  54, 55, 56, 57, 58, 59, 60, 61, 62, 
-					  63, 64, 65, 66, 67, 68, 69, 70, 71, 
-					  72, 73, 74, 75, 76, 77, 78, 79, 80};
-	int target = 1147;
-	cout << inX(puzzle, 2, target) << endl;
+	int col;
+	int row;
+	int puzzle[L] =   {3, 0, 6, 5, 0, 8, 4, 0, 0,
+                       5, 2, 0, 0, 0, 0, 0, 0, 0,
+                       0, 8, 7, 0, 0, 0, 0, 3, 1,
+                       0, 0, 3, 0, 1, 0, 0, 8, 0,
+                       9, 0, 0, 8, 6, 3, 0, 0, 5,
+                       0, 5, 0, 0, 9, 0, 6, 0, 0,
+                       1, 3, 0, 0, 0, 0, 2, 5, 0,
+                       0, 0, 0, 0, 0, 0, 0, 7, 4,
+                       0, 0, 5, 2, 0, 6, 3, 0, 0};
+	int target = 3;
+	cout << inX(puzzle, 8, target) << endl;
+	cout << inY(puzzle, 5, target) << endl;
+	cout << inB(puzzle, 4 - 4%3, 4 - 4%3, target) << endl;
 	return 0;
 }
